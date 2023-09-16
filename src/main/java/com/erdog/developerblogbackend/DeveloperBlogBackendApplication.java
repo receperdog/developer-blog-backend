8cb358d8a7bb0ec1e1815549071167b6a141c939
receptayyiperdogan <receptayyiperdogan8@gmail.com>
2023-09-17 01:18:18 +0300

    Initial commit

---
+++ src/main/java/com/erdog/developerblogbackend/DeveloperBlogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.erdog.developerblogbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloperBlogBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeveloperBlogBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/erdog/developerblogbackend/DeveloperBlogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erdog.developerblogbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeveloperBlogBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
