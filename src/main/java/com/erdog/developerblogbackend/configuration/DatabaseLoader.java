5c73e8c4eb8f1d021afbef72b677a6d2b523aab0
receptayyiperdogan <receptayyiperdogan8@gmail.com>
2023-09-17 03:27:18 +0300

    Update dummy data

--- src/main/java/com/erdog/developerblogbackend/configuration/DatabaseLoader.java
+++ src/main/java/com/erdog/developerblogbackend/configuration/DatabaseLoader.java
@@ -1,14 +1,17 @@
 package com.erdog.developerblogbackend.configuration;
 
+import com.erdog.developerblogbackend.model.Comment;
 import com.erdog.developerblogbackend.model.Post;
 import com.erdog.developerblogbackend.model.User;
 import com.erdog.developerblogbackend.model.enums.PostStatus;
+import com.erdog.developerblogbackend.repository.CommentRepository;
 import com.erdog.developerblogbackend.repository.PostRepository;
 import com.erdog.developerblogbackend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.Date;
 
 @Component
@@ -17,6 +20,7 @@ public class DatabaseLoader implements CommandLineRunner {
 
     private final UserRepository userRepository;
     private final PostRepository postRepository;
+    private final CommentRepository commentRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -28,10 +32,27 @@ public class DatabaseLoader implements CommandLineRunner {
             userRepository.save(alice);
             userRepository.save(jane);
         }
-        if (postRepository.count() == 0){
-            postRepository.save(new Post("John's First Post", "This is the content of John's first post.", "A post by John", john, new Date(), PostStatus.PUBLISHED));
-            postRepository.save(new Post("Thoughts by Jane", "Just some thoughts by Jane.", "Thoughts about life", jane, new Date(), PostStatus.DRAFT));
-            postRepository.save(new Post("Alice's Adventures", "Adventures described by Alice.", "Adventures in wonderland", alice, new Date(), PostStatus.PUBLISHED));
+        if (postRepository.count() == 0) {
+            // Create and save posts first
+            Post johnsPost = new Post("John's First Post", "This is the content of John's first post.", "A post by John", john, new Date(), PostStatus.PUBLISHED, null);
+            Post janesPost = new Post("Thoughts by Jane", "Just some thoughts by Jane.", "Thoughts about life", jane, new Date(), PostStatus.DRAFT, null);
+            Post alicesPost = new Post("Alice's Adventures", "Adventures described by Alice.", "Adventures in wonderland", alice, new Date(), PostStatus.PUBLISHED, null);
+
+            postRepository.save(johnsPost);
+            postRepository.save(janesPost);
+            postRepository.save(alicesPost);
+
+            // Create and associate comments
+            Comment comment1 = new Comment("This is the first comment on John's post.", alice, johnsPost);
+            Comment comment2 = new Comment("This is the second comment on John's post.", jane, johnsPost);
+            Comment comment3 = new Comment("This is a comment on Jane's post.", john, janesPost);
+
+            johnsPost.setComments(Arrays.asList(comment1, comment2));
+            janesPost.setComments(Arrays.asList(comment3));
+
+            // Save the posts again to persist the comments
+            postRepository.save(johnsPost);
+            postRepository.save(janesPost);
         }
     }
 }
